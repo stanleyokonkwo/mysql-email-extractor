b5d11462a29798ab36f80a786324b5bfe3389cd9
Stanley Okonkwo <agrictime@gmail.com>
2022-10-03 06:27:06 -0700

    Create extractor.py

---
+++ extractor.py
@@ -0,0 +1,17 @@
+import re
+ 
+try:
+    file =  open("[UPLOAD YOUR RAW SQL FILE]", "r",encoding='utf8')
+    for line in file:
+        lines = line.strip()
+        emails = re.findall("[0-9a-zA-z]+@[0-9a-zA-z]+\.[0-9a-zA-z]+", lines)
+      
+        savs =  open("CREATEFILE_FOR_SAVING ","a",encoding='utf8')
+        
+        fil = savs.write("\n".join(emails))
+        print(fil)
+        savs.write("\n")
+        savs.close()
+    
+except FileNotFoundError as e:
+    print(e)
